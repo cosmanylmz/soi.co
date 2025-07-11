b10ae7c430ce874bffa8e44df302c547337223ac
Osman YILMAZ <osmanyilmaz@ogr.eskisehir.edu.tr>
2025-07-11 16:32:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-// index.js
+// index.js - Corrected
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// import { createBrowserRouter, RouterProvider } from "react-router-dom"; // <-- Commented out import
-
-// const router = createBrowserRouter([ // <-- Commented out code
-//    {
-//      path: "/",
-//      element: <App />,
-//    },
-// ]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */} {/* <-- Commented out JSX */}
     <App />
   </React.StrictMode>
 );
