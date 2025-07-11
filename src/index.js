a34660d2917119a3b728174170c7b38d77b830da
Osman YILMAZ <osmanyilmaz@ogr.eskisehir.edu.tr>
2025-07-11 16:32:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"; // <-- Commented out import
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
+// const router = createBrowserRouter([ // <-- Commented out code
+//    {
+//      path: "/",
+//      element: <App />,
+//    },
 // ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
+    {/* <RouterProvider router={router} /> */} {/* <-- Commented out JSX */}
     <App />
   </React.StrictMode>
 );
